b2f48d0d22fad3ea0a5f8591856b7bca606141bd
Saleem Khan <saleemkce@gmail.com>
2015-05-07 12:39:52 +0530

    firePHP with simpleXML object inspection added

---
+++ examples/simpleXml.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * exmple with SimpleXML
+ *
+ * SimpleXMLElement::asXML — Return a well-formed XML string based on SimpleXML element
+ * 
+ */
+
+// Include the server library
+require_once('../FirePHP/lib/FirePHPCore/FirePHP.class.php');
+
+// Start output buffering
+//ob_start();
+
+$firephp = FirePHP::getInstance(true);
+
+$firephp->setEnabled(true);  // or FB::
+
+$string = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<CATALOG>
+	<CD>
+		<TITLE>Empire Burlesque</TITLE>
+		<ARTIST>Bob Dylan</ARTIST>
+		<COUNTRY>USA</COUNTRY>
+		<COMPANY>Columbia</COMPANY>
+		<PRICE>10.90</PRICE>
+		<YEAR>1985</YEAR>
+	</CD>
+	<CD>
+		<TITLE>Hide your heart</TITLE>
+		<ARTIST>Bonnie Tyler</ARTIST>
+		<COUNTRY>UK</COUNTRY>
+		<COMPANY>CBS Records</COMPANY>
+		<PRICE>9.90</PRICE>
+		<YEAR>1988</YEAR>
+	</CD>
+	<CD>
+		<TITLE>Greatest Hits</TITLE>
+		<ARTIST>Dolly Parton</ARTIST>
+		<COUNTRY>USA</COUNTRY>
+		<COMPANY>RCA</COMPANY>
+		<PRICE>9.90</PRICE>
+		<YEAR>1982</YEAR>
+	</CD>
+	<CD>
+		<TITLE>Still got the blues</TITLE>
+		<ARTIST>Gary Moore</ARTIST>
+		<COUNTRY>UK</COUNTRY>
+		<COMPANY>Virgin records</COMPANY>
+		<PRICE>10.20</PRICE>
+		<YEAR>1990</YEAR>
+	</CD>
+</CATALOG>
+XML;
+
+$xml = new SimpleXMLElement($string);
+
+$xmlOutput = $xml->asXML();
+
+$firephp->log($xml);
+
+?>
