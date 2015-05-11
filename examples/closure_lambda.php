7f231fe9bf3403c9b4df36c40d37ceb3e090d249
Saleem Khan <saleemkce@gmail.com>
2015-05-11 09:35:24 +0530

    added closure example

---
+++ examples/closure_lambda.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * LAMBDA example
+ */
+// Anonymous function
+// assigned to variable
+$greeting = function () {
+	return "Hello world";
+}
+ 
+// Call function
+echo $greeting();
+// Returns "Hello world"
+
+
+/**
+ * Closure example
+ */ 
+// Create a user
+$user = "Philip";
+ 
+// Create a Closure
+$greeting = function() use ($user) {
+  echo "Hello $user";
+};
+ 
+// Greet the user
+$greeting(); // Returns "Hello Philip"
+
+
+
+/**
+ * Closure with array_walk example
+ */ 
+// An array of names
+$users = array("John", "Jane", "Sally", "Philip");
+ 
+// Pass the array to array_walk
+array_walk($users, function ($name) {
+  echo "Hello $name<br>";
+});
+// Returns
+// -> Hello John
+// -> Hello Jane
+// -> ..
