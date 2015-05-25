ae8590a1bda128e0a566dc3a460b0ab1c7c43dff
Saleem Khan <saleemkce@gmail.com>
2015-05-25 12:02:13 +0530

    modifying array values

--- examples/closure_lambda.php
+++ examples/closure_lambda.php
@@ -33,7 +33,7 @@ $greeting(); // Returns "Hello Philip"
  * Closure with array_walk example
  */ 
 // An array of names
-$users = array("John", "Jane", "Sally", "Philip", "James", "John");
+$users = array("John", "Sally", "Philip", "James", "John");
  
 // Pass the array to array_walk
 array_walk($users, function ($name) {
